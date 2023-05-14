8e9756fce4dd56596a3a4ad235962e080405f195
prabhat-cn <prabhat@capitalnumbers.com>
2023-05-14 15:47:01 +0530

    new operation added

--- src/App.js
+++ src/App.js
@@ -2,19 +2,20 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/HomePage';
-import ViewTodo from './pages/ViewTodo';
-import CreateTodo from './pages/CreateTodo';
+import ViewTodo from './pages/todoApp/ViewTodo';
+import CreateTodo from './pages/todoApp/CreateTodo';
+import Counter from './pages/otherApp/Counter';
 
 function App() {
   return (
     <>
       <div className="App">
-        <h1>React Todo App</h1>
+        <h1>React All Apps</h1>
         <Routes>
-          <Route exact path="/" element={<HomePage />} />
+          <Route exact path="/" element={<Counter />} />
+          <Route exact path="/home" element={<HomePage />} />
           <Route exact path="/view" element={<ViewTodo />} />
           <Route exact path="/create" element={<CreateTodo />} />
-
         </Routes>
       </div>
     </>
---
+++ src/pages/otherApp/Counter.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+
+const Counter = () => {
+  const [number, setNumber] = useState(0);
+
+  const onIncrement = () => {
+    setNumber(number + 1);
+  };
+
+  const onDecrement = () => {
+    setNumber(number - 1);
+    if (number === 0) {
+      setNumber(0);
+    }
+  };
+
+  // console.log('number-->', number);
+
+  localStorage.setItem('countedNumber', JSON.stringify(number));
+
+  const storeCountedNum = JSON.parse(localStorage.getItem('countedNumber'));
+  console.log('storeCountedNum=>', storeCountedNum);
+
+  return (
+    <>
+      <div className="container">
+        <h3>Counter App</h3>
+        <h4 style={{ fontSize: 100 }}>
+          <span>{number ?? storeCountedNum}</span>
+        </h4>
+        <button style={{ fontSize: 20 }} onClick={() => onIncrement()}>
+          Increment
+        </button>
+        &nbsp;&nbsp;&nbsp;
+        <button style={{ fontSize: 20 }} onClick={() => onDecrement()}>
+          Decrement
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default Counter;
