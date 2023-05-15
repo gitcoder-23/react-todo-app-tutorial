2beaf64e166f0ad27b8475673318dab2c50229fe
prabhat-cn <prabhat@capitalnumbers.com>
2023-05-15 20:51:39 +0530

    🚑

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import CreateTodo from './pages/todoApp/CreateTodo';
 import Counter from './pages/otherApp/Counter';
 import SmallCalculator from './pages/otherApp/SmallCalculator';
 import SmallCounterApp from './pages/miniAps/SmallCounterApp';
-import SmallCounterByFourApp from "./pages/myProjectTasks/SmallCounterByFourApp";
+import SmallCounterByFourApp from './pages/myProjectTasks/SmallCounterByFourApp';
 
 function App() {
   return (
@@ -21,7 +21,11 @@ function App() {
           <Route exact path="/counter" element={<Counter />} />
           <Route exact path="/view" element={<ViewTodo />} />
           <Route exact path="/create" element={<CreateTodo />} />
-          <Route exact path="/counter4" element={<SmallCounterByFourApp />} />
+          <Route
+            exact
+            path="/small-counter"
+            element={<SmallCounterByFourApp />}
+          />
         </Routes>
       </div>
     </>
--- src/pages/myProjectTasks/SmallCounterByFourApp.js
+++ src/pages/myProjectTasks/SmallCounterByFourApp.js
@@ -1,27 +1,34 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react';
 
 const SmallCounterByFourApp = () => {
-    const [v, cb ] = useState(0);
-    const onAdd = () => {
-        let newVar = v + 4;
-        cb(newVar);
+  const [newNumber, setNewNumber] = useState(0);
+  const onAdd = () => {
+    let newVar = newNumber + 4;
+    setNewNumber(newVar);
+  };
+  const onMinus = () => {
+    let newVar = newNumber - 4;
+    setNewNumber(newVar);
+    if (newNumber === 0) {
+      setNewNumber(0);
     }
-    const onMinus = () => {
-        let newVar = v - 4;
-        cb(newVar);
-        if (v === 0){
-            cb(0);
-        }
-    }
-    console.log('v-->', v);
+  };
+  console.log('newNumber-->', newNumber);
   return (
-    <div style={{backgroundColor : 'cyan' }}>
+    <div style={{ backgroundColor: 'cyan' }}>
       <h2>SmallCounterByFourApp</h2>
-      <h4 style={{fontSize : 100 }}><span>{v}</span></h4>
-      <button style={{fontSize : 30}} onClick={() => onAdd ()}>ADD</button> &nbsp;&nbsp;&nbsp;&nbsp;
-      <button style={{fontSize : 30}} onClick={() => onMinus ()}>MINUS</button>
+      <h4 style={{ fontSize: 100 }}>
+        <span>{newNumber}</span>
+      </h4>
+      <button style={{ fontSize: 30 }} onClick={() => onAdd()}>
+        ADD
+      </button>{' '}
+      &nbsp;&nbsp;&nbsp;&nbsp;
+      <button style={{ fontSize: 30 }} onClick={() => onMinus()}>
+        MINUS
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default SmallCounterByFourApp
+export default SmallCounterByFourApp;
