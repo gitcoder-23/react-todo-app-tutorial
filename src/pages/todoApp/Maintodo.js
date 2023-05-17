f852f4689ae5a33d0b2412a30219153e5648f1e1
prabhat-cn <prabhat@capitalnumbers.com>
2023-05-17 11:35:18 +0530

    🎉

--- src/pages/todoApp/Maintodo.js
+++ src/pages/todoApp/Maintodo.js
@@ -2,54 +2,122 @@ import React, { useState } from 'react';
 
 const MainTodo = () => {
   const [todoName, setTodoName] = useState('');
+  const [viewData, setViewData] = useState('');
+  const [todoComplete, setTodoComplete] = useState(false);
   const [todoData, setTodoData] = useState([]);
   const [errorMessage, SetErrorMessage] = useState('');
 
   const onChangeInput = (e) => {
     setTodoName(e.target.value);
   };
-  const addTodo = () => {
+  const todoHandleSubmit = (e) => {
+    console.log(e);
+    e.preventDefault();
     if (!todoName || todoName === '') {
       SetErrorMessage('Please add todo');
       setTimeout(() => {
         SetErrorMessage('');
       }, 2000);
     }
-    setTodoData([...todoData, todoName]);
+    const newTodo = {
+      id: new Date().getTime(),
+      todoTxt: todoName,
+      completed: todoComplete,
+    };
+    setTodoData([...todoData, newTodo]);
     setTodoName('');
+    // setTodoData([...todoData].concat(newTodo));
+    // setTodoData([...newTodo, todoName]);
   };
 
-  // console.log('todoName-->', todoName, todoData.length);
+  const toggleComplete = (tId) => {
+    const toggleItem = [...todoData].filter((todo) => {
+      if (todo.id === tId) {
+        todo.completed = !todo.completed;
+      }
+      return todo;
+    });
+    setTodoData(toggleItem);
+  };
+
+  const viewClick = (vData) => {
+    setViewData(vData);
+
+    setTimeout(() => {
+      setViewData('');
+    }, 3000);
+  };
+
+  const editClick = (eData) => {
+    console.log('eData->', eData);
+    setTodoName([...eData]);
+  };
+
+  const deleteClick = (dId) => {
+    if (window.confirm('Do you want?')) {
+      const removeItem = [...todoData].filter((todo) => {
+        return todo.id !== dId;
+      });
+      setTodoData(removeItem);
+    }
+  };
+
+  console.log('todoName-->', todoName, todoData, viewData, todoData.completed);
   return (
     <div>
       <h2>Todo App</h2>
       <div>
-        <input
-          type="text"
-          name="todoName"
-          id="todoName"
-          value={todoName}
-          onChange={onChangeInput}
-          style={{ fontSize: 30 }}
-        />
-        &nbsp;&nbsp;&nbsp;
-        <button
-          type="button"
-          style={{
-            fontSize: 30,
-            width: 'auto',
-            backgroundColor: 'green',
-            color: '#fff',
-          }}
-          onClick={() => addTodo()}
-        >
-          Add
-        </button>
+        <form onSubmit={todoHandleSubmit}>
+          <input
+            type="text"
+            name="todoName"
+            id="todoName"
+            value={todoName}
+            onChange={onChangeInput}
+            style={{ fontSize: 30 }}
+          />
+          &nbsp;&nbsp;&nbsp;
+          <input
+            style={{ fontSize: 50 }}
+            type="checkbox"
+            checked={todoData.completed}
+            onChange={(e) => setTodoComplete(e.target.checked)}
+            // onChange={() => toggleComplete(todoData.id)}
+          />
+          &nbsp;&nbsp;&nbsp;
+          <button
+            type="submit"
+            disabled={!todoName}
+            style={{
+              fontSize: 30,
+              width: 'auto',
+              backgroundColor: !todoName ? 'grey' : 'green',
+              color: '#fff',
+            }}
+          >
+            Add
+          </button>
+        </form>
       </div>
       <div>
         <h4 style={{ color: 'red' }}>{errorMessage}</h4>
       </div>
       &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;
+      {viewData && (
+        <>
+          <div
+            style={{
+              border: '1px solid #000',
+              width: '40%',
+              margin: '0px auto',
+            }}
+          >
+            <h3>View Todo</h3>
+            <h4>{viewData.todoTxt}</h4>
+          </div>
+          &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;
+        </>
+      )}
       {todoData.length === 0 ? (
         <h3 style={{ color: '#000' }}>No todo available!</h3>
       ) : (
@@ -61,17 +129,73 @@ const MainTodo = () => {
           >
             <thead>
               <tr>
-                <th style={{ textDecoration: 'underline' }}>Sl.No</th>
-                <th style={{ textDecoration: 'underline' }}>Todo Name</th>
+                <th style={{ textDecoration: 'underline' }}>Sl.No</th>{' '}
+                &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;
+                <th style={{ textDecoration: 'underline' }}>Todo Name</th>{' '}
+                &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;
+                <th style={{ textDecoration: 'underline' }}>Action</th>{' '}
+                &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;
               </tr>
             </thead>
             &nbsp;&nbsp;&nbsp;
             {todoData &&
               todoData.map((tData, indx) => (
-                <tbody>
+                <tbody key={tData.id}>
                   <tr>
-                    <td>{indx + 1}</td>
-                    <td>{tData}</td>
+                    <td
+                      style={{
+                        fontSize: 18,
+                      }}
+                    >
+                      {indx + 1}
+                    </td>{' '}
+                    &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;
+                    <td
+                      style={{
+                        fontSize: 18,
+                      }}
+                    >
+                      {tData.todoTxt}
+                    </td>{' '}
+                    &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;
+                    <td>
+                      <button
+                        type="button"
+                        style={{
+                          backgroundColor: 'green',
+                          color: '#fff',
+                          fontSize: 18,
+                        }}
+                        onClick={() => viewClick(tData)}
+                      >
+                        View
+                      </button>
+                      &nbsp;&nbsp;&nbsp;
+                      <button
+                        type="button"
+                        style={{
+                          backgroundColor: '#807800',
+                          color: '#fff',
+                          fontSize: 18,
+                        }}
+                        onClick={() => editClick(tData)}
+                      >
+                        Edit
+                      </button>
+                      &nbsp;&nbsp;&nbsp;
+                      <button
+                        type="button"
+                        style={{
+                          backgroundColor: '#807800',
+                          color: '#fff',
+                          fontSize: 18,
+                        }}
+                        onClick={() => deleteClick(tData.id)}
+                      >
+                        Delete
+                      </button>
+                    </td>{' '}
+                    &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;
                   </tr>
                 </tbody>
               ))}
