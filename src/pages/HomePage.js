743b007465d9b8964f6f723a81e49be2ec704749
Suman-89 <suman.mukherjee01@gmail.com>
2023-07-14 02:09:01 +0530

    edit op

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
-import EditModal from "./todoApp/homepageModal/EditModal";
+// import EditModal from "./todoApp/homepageModal/EditModal";
 
 const HomePage = () => {
   const [dataInput, setDataInput] = useState("");
   const [dataInArray, setDataInArray] = useState([]);
-  // const [vData, setVdata] = useState({});
+  const [vData, setVdata] = useState('');
+  const [edId, setEdId] = useState("");
   const [errorMsg, setErrorMsg] = useState("");
 
   const inputTodo = (e) => {
@@ -30,22 +31,35 @@ const HomePage = () => {
   };
   // console.log("dataInArray-->", dataInArray);
 
-  const delFunc = (del)=>{
-    console.log('del-->',del);
-    if(window.confirm('Do you want to delete ?')){
-      const delItems = [...dataInArray].filter((arrayData)=>{
-        console.log('arrayData-->',arrayData.id);
-        return arrayData.id !== del ;
-      })
+  const delFunc = (del) => {
+    console.log("del-->", del);
+    if (window.confirm("Do you want to delete ?")) {
+      const delItems = [...dataInArray].filter((arrayData) => {
+        console.log("arrayData-->", arrayData.id);
+        return arrayData.id !== del;
+      });
       setDataInArray(delItems);
     }
-  }
+  };
 
-  const viewItem = (item)=>{
-    console.log('item-->',item);
-  }
-  // console.log('vData-->',vData);
+  const viewItem = (item, edId) => {
+    console.log("item-->", item, "edId-->", edId);
+    setVdata(item);
+    setEdId(edId);
+  };
+  console.log("vData-->", vData, "edId-->", edId);
 
+  const saveEdit = eIndex => {
+    console.log('eIndex-->',eIndex);
+    const updateData = [...dataInArray].map((upData,upIndex) => {
+      console.log('upData-->',upData);
+      if(upIndex === eIndex){
+        upData.todoItem = vData ;
+      }
+      return upData;
+    })
+    setDataInArray(updateData);
+  }
 
   return (
     <div className="container my-4">
@@ -87,46 +101,115 @@ const HomePage = () => {
               </button>
             </div>
           </div>
-          {dataInArray.length === 0 ? (<span>No todo item</span>):(
-             <table className="table">
-             <thead>
-               <tr>
-                 <th scope="col">No.</th>
-                 <th scope="col">Todo Item</th>
-                 <th scope="col">Actions</th>
-               </tr>
-             </thead>
-           
-           {dataInArray &&
-             dataInArray.map((data, index) => (
-               <tbody key={index}>
-                 <tr>
-                   <th scope="row">{index+1}.</th>
-                   <td>{data.todoItem}</td>
-                   <td>
-                     <div
-                       className="btn-group"
-                       role="group"
-                       aria-label="Basic mixed styles example"
-                     >
-                       <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal"
-                       onClick={()=>viewItem(data)}>
-                         Edit
-                       </button>
-                       <EditModal/>
-                       <button type="button" className="btn btn-danger"
-                       onClick={()=>delFunc(data.id)}
-                       >
-                         Delete
-                       </button>
-                     </div>
-                   </td>
-                 </tr>
-               </tbody>
-             ))}
-             </table>
+          {dataInArray.length === 0 ? (
+            <span>No todo item</span>
+          ) : (
+            <table className="table">
+              <thead>
+                <tr>
+                  <th scope="col">No.</th>
+                  <th scope="col">Todo Item</th>
+                  <th scope="col">Actions</th>
+                </tr>
+              </thead>
+
+              {dataInArray &&
+                dataInArray.map((data, index) => (
+                  <tbody key={index}>
+                    <tr>
+                      <th scope="row">{index + 1}.</th>
+                      <td>{data.todoItem}</td>
+                      <td>
+                        <div
+                          className="btn-group"
+                          role="group"
+                          aria-label="Basic mixed styles example"
+                        >
+                          <button
+                            type="button"
+                            className="btn btn-primary"
+                            data-bs-toggle="modal"
+                            data-bs-target="#exampleModal"
+                            onClick={() => viewItem(data.todoItem, data.id)}
+                          >
+                            Edit
+                          </button>
+                          <div
+                            className="modal fade"
+                            id="exampleModal"
+                            tabIndex="-1"
+                            aria-labelledby="exampleModalLabel"
+                            aria-hidden="true"
+                          >
+                            <div className="modal-dialog">
+                              <div className="modal-content">
+                                <div className="modal-header">
+                                  <h1
+                                    className="modal-title fs-5"
+                                    id="exampleModalLabel"
+                                  >
+                                    Modal title
+                                  </h1>
+                                  <button
+                                    type="button"
+                                    className="btn-close"
+                                    data-bs-dismiss="modal"
+                                    aria-label="Close"
+                                  ></button>
+                                </div>
+                                <div className="modal-body">
+                                  <div className="input-group mb-3">
+                                    <span
+                                      className="input-group-text"
+                                      id="inputGroup-sizing-default"
+                                    >
+                                      Todo_Item :
+                                    </span>
+                                    <input
+                                      type="text"
+                                      className="form-control"
+                                      aria-label="Sizing example input"
+                                      aria-describedby="inputGroup-sizing-default"
+                                      value={vData}
+                                      onChange={(e)=>setVdata(e.target.value)}
+                                    />
+                                  </div>
+                                </div>
+                                <div className="modal-footer">
+                                  <button
+                                    type="button"
+                                    className="btn btn-secondary"
+                                    data-bs-dismiss="modal"
+                                  >
+                                    Close
+                                  </button>
+                                  <button
+                                    type="button"
+                                    className="btn btn-primary"
+                                    data-bs-dismiss="modal"
+                                    onClick={()=>saveEdit(index)}
+                                  >
+                                    Save changes
+                                  </button>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                          {/* <EditModal vData={vData}/> */}
+                          <button
+                            type="button"
+                            className="btn btn-danger"
+                            onClick={() => delFunc(data.id)}
+                          >
+                            Delete
+                          </button>
+                        </div>
+                      </td>
+                    </tr>
+                  </tbody>
+                ))}
+            </table>
           )}
-         
         </div>
       </div>
     </div>
--- src/pages/todoApp/homepageModal/EditModal.js
+++ src/pages/todoApp/homepageModal/EditModal.js
@@ -1,38 +1,51 @@
 import React from "react";
 
-const EditModal = () => {
+const EditModal = ({ vData }) => {
   return (
     <div>
       <div
-        class="modal fade"
+        className="modal fade"
         id="exampleModal"
-        tabindex="-1"
+        tabIndex="-1"
         aria-labelledby="exampleModalLabel"
         aria-hidden="true"
       >
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h1 class="modal-title fs-5" id="exampleModalLabel">
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5" id="exampleModalLabel">
                 Modal title
               </h1>
               <button
                 type="button"
-                class="btn-close"
+                className="btn-close"
                 data-bs-dismiss="modal"
                 aria-label="Close"
               ></button>
             </div>
-            <div class="modal-body">...</div>
-            <div class="modal-footer">
+            <div className="modal-body">
+              <div className="input-group mb-3">
+                <span className="input-group-text" id="inputGroup-sizing-default">
+                  Todo_Item :
+                </span>
+                <input
+                  type="text"
+                  className="form-control"
+                  aria-label="Sizing example input"
+                  aria-describedby="inputGroup-sizing-default"
+                  value={vData}
+                />
+              </div>
+            </div>
+            <div className="modal-footer">
               <button
                 type="button"
-                class="btn btn-secondary"
+                className="btn btn-secondary"
                 data-bs-dismiss="modal"
               >
                 Close
               </button>
-              <button type="button" class="btn btn-primary">
+              <button type="button" className="btn btn-primary" data-bs-dismiss="modal">
                 Save changes
               </button>
             </div>
